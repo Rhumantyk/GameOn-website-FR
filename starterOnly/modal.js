e74a530db7dbc1a9220712aef05fdb7e2c08e405
Pierre LEONARD <leonardpierrepro@gmail.com>
2021-09-28 10:18:25 +0200

    Changment couleur dans class wrong css, et la variable errorColor dans JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -38,7 +38,7 @@ function closeModal() // Fermeture modal
 }
 
 // Variable erreurs
-let errorColor = "inset 0 0 0 3px red"; // Liseré rouge dans input (laissant du blanc entre l'input et ce liseré, sans la variable "errorBorder")
+let errorColor = "inset 0 0 0 3px #e54858"; // Liseré rouge dans input (laissant du blanc entre l'input et ce liseré, sans la variable "errorBorder")
 let errorBorder = "none" // Empêche le liseré blanc entre errorColor et l'input.
 
 // Issues#2 : Implémenter entrées du formulaire.
